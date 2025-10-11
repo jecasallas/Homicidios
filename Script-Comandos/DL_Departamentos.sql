d0d8e9c53700405cc94f91ce4d9ce9588d613bbd
jecasallas <jcasallasca01@uninpahu.edu.co>
2025-10-11 14:47:48 -0500

    Script DL_Departamentos

---
+++ Script-Comandos/DL_Departamentos.sql
@@ -0,0 +1,7 @@
+CREATE TABLE DataLake.dbo.DL_Departamentos (
+    COD_DPTO tinyint NULL,
+    NOM_DPTO varchar(56) NULL,
+    LATITUD float NULL,
+    LONGITUD float NULL,
+    Geo_Departamento varchar(32) NULL
+);
