c598282f461b2e285c6445828a58284a2687f68e
jecasallas <jcasallasca01@uninpahu.edu.co>
2025-10-11 14:49:49 -0500

    Script DL_Municipios

---
+++ Script-Comandos/DL_Municipios.sql
@@ -0,0 +1,10 @@
+CREATE TABLE DataLake.dbo.DL_Municipios (
+	COD_DPTO tinyint NULL,
+	NOM_DPTO varchar(100) NULL,
+	COD_MPIO int NULL,
+	NOM_MPIO varchar(100) NULL,
+	TIPO varchar(50) NULL,
+	LATITUD float NULL,
+	LONGITUD float NULL,
+	Geo_Municipio varchar(100) NULL
+);
